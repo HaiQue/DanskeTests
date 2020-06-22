d9f09198c38c94d793a69dc3f4307191a4828c83
HaiQue <43703237+HaiQue@users.noreply.github.com>
2020-06-22 11:14:53 +0300

    Create EvenFibonnaci.cs

---
+++ EvenFibonnaci.cs
@@ -0,0 +1,19 @@
+        public static int EvenFibonnaci(int n)
+        {
+            int[] fibonnaci = new int[n];
+            int evenSum = 0;
+            int normalSum = 0; // optional for In the code comments leave the actual sum you found so we can compare whether you found the right answer.
+       
+            fibonnaci[0] = 0;
+            fibonnaci[1] = 1;
+
+            for (int i = 2; i < n; i++)
+            {
+                fibonnaci[i] = fibonnaci[i - 1] + fibonnaci[i - 2];
+                normalSum += fibonnaci[i]; // optional for the actual sum
+                if (fibonnaci[i] % 2 == 0) // If number is even
+                    evenSum += fibonnaci[i];
+            }
+
+            return evenSum; 
+        }
